59b3855e6d4cc6dfc5f0f1a6948f5602c0668e61
Ningxin Hu <ningxin.hu@intel.com>
2020-10-16 15:25:43 +0800

    Add more shapes into dynamic_shape.js

--- code/samples/dynamic_shape.js
+++ code/samples/dynamic_shape.js
@@ -11,16 +11,19 @@ const model = builder.createModel({c});
 
 const compiledModel = await model.compile();
 
-const shapeA = [3, 4];
-const shapeB = [4, 3];
-const bufferA = new Float32Array(sizeOfShape(shapeA)).fill(0.5);
-const bufferB = new Float32Array(sizeOfShape(shapeB)).fill(0.5);
+async function compute(shapeA, shapeB) {
+  const bufferA = new Float32Array(sizeOfShape(shapeA)).fill(0.5);
+  const bufferB = new Float32Array(sizeOfShape(shapeB)).fill(0.5);
 
-// Specify the shape of inputs when computing.
-const inputs = {
-  'a': {buffer: bufferA, dimensions: shapeA},
-  'b': {buffer: bufferB, dimensions: shapeB},
-};
-const outputs = await compiledModel.compute(inputs);
-console.log(`shape: [${outputs.c.dimensions}]`);
-console.log(`values: ${outputs.c.buffer}`);
+  // Specify the shape of inputs when computing.
+  const inputs = {
+    'a': {buffer: bufferA, dimensions: shapeA},
+    'b': {buffer: bufferB, dimensions: shapeB},
+  };
+  const outputs = await compiledModel.compute(inputs);
+  console.log(`shape: [${outputs.c.dimensions}], values: ${outputs.c.buffer}`);
+}
+
+await compute([3, 4], [4, 3]);
+await compute([4, 4], [4, 4]);
+await compute([5, 4], [4, 5]);
