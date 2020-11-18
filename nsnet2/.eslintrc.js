1ca2bfa23a5551108231278915e729d241441c0f
Ningxin Hu <ningxin.hu@intel.com>
2020-11-18 15:33:42 +0800

    Use audio tag and recorder.js for audio playback
    recorderWorker.js is from https://github.com/mattdiamond/Recorderjs/blob/ac0eb8a7c2601fc4ec1cbd1b1ee49f45a6c79580/recorderWorker.js

--- nsnet2/.eslintrc.js
+++ nsnet2/.eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  ignorePatterns: ['libs/'],
   globals: {
     'tf': 'readonly',
   },
--- nsnet2/main.js
+++ nsnet2/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import {Denoiser} from './denoiser.js';
-import {AudioPlayer} from './audio_player.js';
 
 const sampleRate = 16000;
 const batchSize = 1;
@@ -10,12 +9,28 @@ const defaultFrames = 100;
 let denoiser;
 let audioData;
 let denoisedAudioData = [];
-const playDenoisedButton = document.getElementById('play-denoised');
-const playOriginalButton = document.getElementById('play-original');
-const denoisedAudioPlayer =
-    new AudioPlayer(sampleRate, playDenoisedButton, 'the denoised audio');
-const originalAudioPlayer =
-    new AudioPlayer(sampleRate, playOriginalButton, 'the original audio');
+
+const originalAudio = document.getElementById('original-audio');
+const denoisedAudio = document.getElementById('denoised-audio');
+
+originalAudio.onplay = () => {
+  denoisedAudio.pause();
+};
+
+denoisedAudio.onplay = () => {
+  originalAudio.pause();
+};
+
+const recorderWorker = new Worker('./libs/recorderWorker.js');
+recorderWorker.postMessage({
+  command: 'init',
+  config: {sampleRate, numChannels: 1},
+});
+
+recorderWorker.onmessage = function( e ) {
+  const blob = e.data;
+  denoisedAudio.src = URL.createObjectURL(blob);
+};
 
 export async function main() {
   try {
@@ -46,69 +61,82 @@ const fileInputLabel = document.getElementById('file-input-label');
 const fileInput = document.getElementById('file-input');
 fileInput.addEventListener('input', (event) => {
   try {
-    originalAudioPlayer.stop();
-    denoisedAudioPlayer.stop();
-    playOriginalButton.setAttribute('disabled', true);
+    originalAudio.pause();
+    denoisedAudio.pause();
+    originalAudio.setAttribute('disabled', true);
     fileInput.setAttribute('disabled', true);
     const input = event.target;
-    const reader = new FileReader();
-    reader.onload = async function() {
-      playDenoisedButton.setAttribute('disabled', true);
-      const arrayBuffer = reader.result;
+    const bufferReader = new FileReader();
+    bufferReader.onload = async function(e) {
+      denoisedAudio.setAttribute('disabled', true);
+      const arrayBuffer = e.target.result;
       const audioContext = new AudioContext({sampleRate});
       const start = performance.now();
       const decoded = await audioContext.decodeAudioData(arrayBuffer);
       console.log(`decode time: ${performance.now() - start}`);
       audioData = decoded.getChannelData(0);
-      playOriginalButton.removeAttribute('disabled');
-      originalAudioPlayer.play(new Float32Array(audioData));
       denoisedAudioData = [];
       await denoiser.process(audioData, (data) => {
         denoisedAudioData = denoisedAudioData.concat(Array.from(data));
       });
-      playDenoisedButton.removeAttribute('disabled');
+      denoisedAudio.removeAttribute('disabled');
       fileInput.removeAttribute('disabled');
+      // Send the denoised audio data for wav encoding.
+      recorderWorker.postMessage({
+        command: 'record',
+        buffer: [new Float32Array(denoisedAudioData)],
+      });
+      recorderWorker.postMessage({
+        command: 'exportWAV',
+        type: 'audio/wav',
+      });
     };
-    reader.readAsArrayBuffer(input.files[0]);
+    bufferReader.readAsArrayBuffer(input.files[0]);
+    const fileReader = new FileReader();
+    fileReader.onload = function(e) {
+      originalAudio.src = e.target.result;
+      originalAudio.removeAttribute('disabled');
+    };
+    fileReader.readAsDataURL(input.files[0]);
   } catch (error) {
     console.log(error);
     addWarning(error.message);
   }
 });
 
-playOriginalButton.onclick = () => {
-  try {
-    if (playOriginalButton.state === 'Pause') {
-      originalAudioPlayer.pause();
-    } else if (playOriginalButton.state === 'Resume') {
-      denoisedAudioPlayer.pause();
-      originalAudioPlayer.resume();
-    } else if (playOriginalButton.state === 'Play') {
-      denoisedAudioPlayer.pause();
-      originalAudioPlayer.play(new Float32Array(audioData));
-    }
-  } catch (error) {
-    console.log(error);
-    addWarning(error.message);
-  }
-};
+// playOriginalButton.onclick = () => {
+//   try {
+//     if (playOriginalButton.state === 'Pause') {
+//       originalAudioPlayer.pause();
+//     } else if (playOriginalButton.state === 'Resume') {
+//       denoisedAudioPlayer.pause();
+//       originalAudioPlayer.resume();
+//     } else if (playOriginalButton.state === 'Play') {
+//       denoisedAudioPlayer.pause();
+//       originalAudioPlayer.play(new Float32Array(audioData));
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     addWarning(error.message);
+//   }
+// };
 
-playDenoisedButton.onclick = () => {
-  try {
-    if (playDenoisedButton.state === 'Pause') {
-      denoisedAudioPlayer.pause();
-    } else if (playDenoisedButton.state === 'Resume') {
-      originalAudioPlayer.pause();
-      denoisedAudioPlayer.resume();
-    } else if (playDenoisedButton.state === 'Play') {
-      originalAudioPlayer.pause();
-      denoisedAudioPlayer.play(new Float32Array(denoisedAudioData));
-    }
-  } catch (error) {
-    console.log(error);
-    addWarning(error.message);
-  }
-};
+// playDenoisedButton.onclick = () => {
+//   try {
+//     if (playDenoisedButton.state === 'Pause') {
+//       denoisedAudioPlayer.pause();
+//     } else if (playDenoisedButton.state === 'Resume') {
+//       originalAudioPlayer.pause();
+//       denoisedAudioPlayer.resume();
+//     } else if (playDenoisedButton.state === 'Play') {
+//       originalAudioPlayer.pause();
+//       denoisedAudioPlayer.play(new Float32Array(denoisedAudioData));
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     addWarning(error.message);
+//   }
+// };
 
 function addWarning(msg) {
   const div = document.createElement('div');
