3aa1e5eb8e8d38ea221057579fa5fb82c452860b
mingmingtasd <mingming1.xu@intel.com>
2021-06-02 09:09:41 +0800

    remove reshaped bias of gemm

--- image_classification/mobilenet_nchw.js
+++ image_classification/mobilenet_nchw.js
@@ -47,8 +47,7 @@ export class MobileNetV2Nchw {
     const weights = await buildConstantByNpy(this.builder_, weightsName);
     const biasName = prefix + '_bias.npy';
     const bias = await buildConstantByNpy(this.builder_, biasName);
-    const reshapedBias = this.builder_.reshape(bias, [1, -1]);
-    const options = {c: reshapedBias, bTranspose: true};
+    const options = {c: bias, bTranspose: true};
     return this.builder_.gemm(input, weights, options);
   }
 
