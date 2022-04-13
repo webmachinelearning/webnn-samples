a342be7baf2b29ddd7b4f3c687657cbd558937d9
Belem Zhang <belem.zhang@intel.com>
2022-04-13 12:11:25 +0800

    Revert "network performance - update the model file urls to amazon s3 (#134)" (#135)
    This reverts commit 0607f3c2b474187c853cf64b8982d7c36ab01309.

--- image_classification/mobilenet_nhwc.js
+++ image_classification/mobilenet_nhwc.js
@@ -9,7 +9,7 @@ export class MobileNetV2Nhwc {
   constructor() {
     this.builder_ = null;
     this.graph_ = null;
-    this.weightsUrl_ = 'https://d3i5xkfad89fac.cloudfront.net/test-data/models/mobilenetv2_nhwc/weights/';
+    this.weightsUrl_ = '../test-data/models/mobilenetv2_nhwc/weights/';
     this.inputOptions = {
       mean: [127.5, 127.5, 127.5],
       std: [127.5, 127.5, 127.5],
