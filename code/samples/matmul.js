aaa9ff168717716a6199927ad044085dd86da7f7
Honry <wanming.lin@intel.com>
2024-02-18 08:13:12 +0000

    Deploying to gh-pages from @ webmachinelearning/webnn-samples@c894ab00eb4457720db28ebe1c209dfa8433a591 🚀

--- code/samples/matmul.js
+++ code/samples/matmul.js
@@ -3,12 +3,12 @@ const context = await navigator.ml.createContext();
 // The following code multiplies matrix a [3, 4] with matrix b [4, 3]
 // into matrix c [3, 3].
 const builder = new MLGraphBuilder(context);
-const descA = {type: 'float32', dataType: 'float32', dimensions: [3, 4]};
+const descA = {dataType: 'float32', dimensions: [3, 4]};
 const a = builder.input('a', descA);
-const descB = {type: 'float32', dataType: 'float32', dimensions: [4, 3]};
+const descB = {dataType: 'float32', dimensions: [4, 3]};
 const bufferB = new Float32Array(sizeOfShape(descB.dimensions)).fill(0.5);
 const b = builder.constant(descB, bufferB);
-const c = builder.matmul(a, b);
+const c = builder.gemm(a, b);
 
 const graph = await builder.build({c});
 const bufferA = new Float32Array(sizeOfShape(descA.dimensions)).fill(0.5);
