897226c0f3728b8f98e96dd01b5b7cc42f65c6dc
Wanming Lin <wanming.lin@intel.com>
2024-06-11 09:55:15 +0800

    Unfuse activation in facenet_nchw.js

--- face_recognition/facenet_nchw.js
+++ face_recognition/facenet_nchw.js
@@ -41,9 +41,7 @@ export class FaceNetNchw {
         bias: bias,
       };
     }
-    if (relu) {
-      options.activation = this.builder_.relu();
-    }
+
     // WebNN spec drops autoPad support, compute the explicit padding instead.
     if (options.autoPad == 'same-upper') {
       options.padding =
@@ -52,7 +50,8 @@ export class FaceNetNchw {
             /* oihw */[weights.shape()[2], weights.shape()[3]],
             options.strides, options.dilations, options.autoPad);
     }
-    return this.builder_.conv2d(input, weights, options);
+    const conv2d = this.builder_.conv2d(input, weights, options);
+    return relu ? this.builder_.relu(conv2d) : conv2d;
   }
 
   async buildBlock35_(input, indice, weightsSuffixes) {
