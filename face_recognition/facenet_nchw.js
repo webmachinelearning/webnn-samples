e82c76893794b35730b4c9bd4068ae8023fc9640
Reilly Grant <reillyg@google.com>
2024-07-02 23:53:31 +0000

    Parallelize loading of FaceNet model weights

--- face_recognition/facenet_nchw.js
+++ face_recognition/facenet_nchw.js
@@ -31,9 +31,10 @@ export class FaceNetNchw {
       input, weightsSuffix, biasPrefix, options = undefined, relu = true) {
     const weightsName =
         `${this.weightsUrl_}/const_fold_opt__${weightsSuffix}.npy`;
-    const weights = await buildConstantByNpy(this.builder_, weightsName);
     const biasName = `${this.weightsUrl_}/${biasPrefix}_Conv2D_bias.npy`;
-    const bias = await buildConstantByNpy(this.builder_, biasName);
+    const [weights, bias] = await Promise.all(
+        [weightsName, biasName].map(
+            name => buildConstantByNpy(this.builder_, name)));
     if (options !== undefined) {
       options.bias = bias;
     } else {
@@ -41,6 +42,8 @@ export class FaceNetNchw {
         bias: bias,
       };
     }
+
+    input = await input;
 
     // WebNN spec drops autoPad support, compute the explicit padding instead.
     if (options.autoPad == 'same-upper') {
@@ -55,65 +58,68 @@ export class FaceNetNchw {
   }
 
   async buildBlock35_(input, indice, weightsSuffixes) {
-    const branch0 = await this.buildConv_(
+    const branch0 = this.buildConv_(
         input, weightsSuffixes[0], `Block35_${indice}_Branch_0_Conv2d_1x1`);
-    const branch1_0 = await this.buildConv_(
+    const branch1_0 = this.buildConv_(
         input, weightsSuffixes[1], `Block35_${indice}_Branch_1_Conv2d_0a_1x1`);
-    const branch1_1 = await this.buildConv_(
+    const branch1_1 = this.buildConv_(
         branch1_0, weightsSuffixes[2],
         `Block35_${indice}_Branch_1_Conv2d_0b_3x3`, {autoPad});
-    const branch2_0 = await this.buildConv_(
+    const branch2_0 = this.buildConv_(
         input, weightsSuffixes[3], `Block35_${indice}_Branch_2_Conv2d_0a_1x1`);
-    const branch2_1 = await this.buildConv_(
+    const branch2_1 = this.buildConv_(
         branch2_0, weightsSuffixes[4],
         `Block35_${indice}_Branch_2_Conv2d_0b_3x3`, {autoPad});
-    const branch2_2 = await this.buildConv_(
+    const branch2_2 = this.buildConv_(
         branch2_1, weightsSuffixes[5],
         `Block35_${indice}_Branch_2_Conv2d_0c_3x3`, {autoPad});
 
-    const concat = this.builder_.concat([branch0, branch1_1, branch2_2], 1);
-    const conv = await this.buildConv_(
+    const concat = Promise.all([branch0, branch1_1, branch2_2])
+        .then((inputs) => this.builder_.concat(inputs, 1));
+    const conv = this.buildConv_(
         concat, weightsSuffixes[6], `Block35_${indice}_Conv2d_1x1`, {}, false);
 
-    return this.builder_.relu(this.builder_.add(input, conv));
+    return this.builder_.relu(this.builder_.add(await input, await conv));
   }
 
   async buildBlock17_(input, indice, weightsSuffixes) {
-    const branch0 = await this.buildConv_(
+    const branch0 = this.buildConv_(
         input, weightsSuffixes[0], `Block17_${indice}_Branch_0_Conv2d_1x1`);
-    const branch1_0 = await this.buildConv_(
+    const branch1_0 = this.buildConv_(
         input, weightsSuffixes[1], `Block17_${indice}_Branch_1_Conv2d_0a_1x1`);
-    const branch1_1 = await this.buildConv_(
+    const branch1_1 = this.buildConv_(
         branch1_0, weightsSuffixes[2],
         `Block17_${indice}_Branch_1_Conv2d_0b_1x7`, {autoPad});
-    const branch1_2 = await this.buildConv_(
+    const branch1_2 = this.buildConv_(
         branch1_1, weightsSuffixes[3],
         `Block17_${indice}_Branch_1_Conv2d_0c_7x1`, {autoPad});
 
-    const concat = this.builder_.concat([branch0, branch1_2], 1);
-    const conv = await this.buildConv_(
+    const concat = Promise.all([branch0, branch1_2])
+        .then((inputs) => this.builder_.concat(inputs, 1));
+    const conv = this.buildConv_(
         concat, weightsSuffixes[4], `Block17_${indice}_Conv2d_1x1`, {}, false);
 
-    return this.builder_.relu(this.builder_.add(input, conv));
+    return this.builder_.relu(this.builder_.add(await input, await conv));
   }
 
   async buildBlock8_(input, indice, weightsSuffixes, relu = true) {
-    const branch0 = await this.buildConv_(
+    const branch0 = this.buildConv_(
         input, weightsSuffixes[0], `Block8_${indice}_Branch_0_Conv2d_1x1`);
-    const branch1_0 = await this.buildConv_(
+    const branch1_0 = this.buildConv_(
         input, weightsSuffixes[1], `Block8_${indice}_Branch_1_Conv2d_0a_1x1`);
-    const branch1_1 = await this.buildConv_(
+    const branch1_1 = this.buildConv_(
         branch1_0, weightsSuffixes[2],
         `Block8_${indice}_Branch_1_Conv2d_0b_1x3`, {autoPad});
-    const branch1_2 = await this.buildConv_(
+    const branch1_2 = this.buildConv_(
         branch1_1, weightsSuffixes[3],
         `Block8_${indice}_Branch_1_Conv2d_0c_3x1`, {autoPad});
 
-    const concat = this.builder_.concat([branch0, branch1_2], 1);
-    const conv = await this.buildConv_(
+    const concat = Promise.all([branch0, branch1_2])
+        .then((inputs) => this.builder_.concat(inputs, 1));
+    const conv = this.buildConv_(
         concat, weightsSuffixes[4], `Block8_${indice}_Conv2d_1x1`, {}, false);
 
-    let result = this.builder_.add(input, conv);
+    let result = this.builder_.add(await input, await conv);
 
     if (relu) {
       result = this.builder_.relu(result);
@@ -122,11 +128,11 @@ export class FaceNetNchw {
   }
 
   async buildGemm_(input) {
-    const weights = await buildConstantByNpy(this.builder_,
+    const weights = buildConstantByNpy(this.builder_,
         `${this.weightsUrl_}/const_fold_opt__888.npy`);
-    const bias = await buildConstantByNpy(this.builder_,
+    const bias = buildConstantByNpy(this.builder_,
         `${this.weightsUrl_}/Bottleneck_MatMul_bias.npy`);
-    return this.builder_.gemm(input, weights, {c: bias});
+    return this.builder_.gemm(input, await weights, {c: await bias});
   }
 
   async load(contextOptions) {
@@ -139,107 +145,110 @@ export class FaceNetNchw {
 
     const poolOptions = {windowDimensions: [3, 3], strides};
 
-    const conv0 = await this.buildConv_(
+    const conv0 = this.buildConv_(
         input, '1070', 'Conv2d_1a_3x3', {strides});
-    const conv1 = await this.buildConv_(conv0, '1146', 'Conv2d_2a_3x3');
-    const conv2 = await this.buildConv_(
+    const conv1 = this.buildConv_(conv0, '1146', 'Conv2d_2a_3x3');
+    const conv2 = this.buildConv_(
         conv1, '1068', 'Conv2d_2b_3x3', {autoPad});
 
-    const pool0 = this.builder_.maxPool2d(conv2, poolOptions);
+    const pool0 = conv2
+        .then((conv2) => this.builder_.maxPool2d(conv2, poolOptions));
 
-    const conv3 = await this.buildConv_(pool0, '1058', 'Conv2d_3b_1x1');
-    const conv4 = await this.buildConv_(conv3, '1045', 'Conv2d_4a_3x3');
-    const conv5 = await this.buildConv_(
+    const conv3 = this.buildConv_(pool0, '1058', 'Conv2d_3b_1x1');
+    const conv4 = this.buildConv_(conv3, '1045', 'Conv2d_4a_3x3');
+    const conv5 = this.buildConv_(
         conv4, '976', 'Conv2d_4b_3x3', {strides});
 
     // Block 35
-    const block35_1 = await this.buildBlock35_(
+    const block35_1 = this.buildBlock35_(
         conv5, 1, ['930', '1073', '912', '1134', '1127', '921', '959']);
-    const block35_2 = await this.buildBlock35_(
+    const block35_2 = this.buildBlock35_(
         block35_1, 2, ['1005', '1036', '902', '925', '1016', '1133', '1059']);
-    const block35_3 = await this.buildBlock35_(
+    const block35_3 = this.buildBlock35_(
         block35_2, 3, ['1046', '1115', '1020', '1091', '958', '948', '891']);
-    const block35_4 = await this.buildBlock35_(
+    const block35_4 = this.buildBlock35_(
         block35_3, 4, ['1053', '1151', '919', '961', '1111', '1124', '900']);
-    const block35_5 = await this.buildBlock35_(
+    const block35_5 = this.buildBlock35_(
         block35_4, 5, ['1083', '1100', '1015', '1082', '1099', '1125', '1081']);
 
     // Mixed 6a branches
-    const mixed6a_branch0 = await this.buildConv_(
+    const mixed6a_branch0 = this.buildConv_(
         block35_5, '1079', 'Mixed_6a_Branch_0_Conv2d_1a_3x3', {strides});
-    const mixed6a_pool = this.builder_.maxPool2d(block35_5, poolOptions);
-    const mixed6a_branch1_0 = await this.buildConv_(
+    const mixed6a_pool = block35_5
+        .then((block35_5) => this.builder_.maxPool2d(block35_5, poolOptions));
+    const mixed6a_branch1_0 = this.buildConv_(
         block35_5, '1072', 'Mixed_6a_Branch_1_Conv2d_0a_1x1');
-    const mixed6a_branch1_1 = await this.buildConv_(
+    const mixed6a_branch1_1 = this.buildConv_(
         mixed6a_branch1_0, '1022',
         'Mixed_6a_Branch_1_Conv2d_0b_3x3', {autoPad});
-    const mixed6a_branch1_2 = await this.buildConv_(
+    const mixed6a_branch1_2 = this.buildConv_(
         mixed6a_branch1_1, '1012',
         'Mixed_6a_Branch_1_Conv2d_1a_3x3', {strides});
-    const mixed6a = this.builder_.concat(
-        [mixed6a_branch0, mixed6a_branch1_2, mixed6a_pool], 1);
+    const mixed6a = Promise.all([mixed6a_branch0, mixed6a_branch1_2, mixed6a_pool])
+        .then((inputs) => this.builder_.concat(inputs, 1));
 
     // Block 17
-    const block17_1 = await this.buildBlock17_(
+    const block17_1 = this.buildBlock17_(
         mixed6a, 1, ['1119', '1112', '989', '1095', '952']);
-    const block17_2 = await this.buildBlock17_(
+    const block17_2 = this.buildBlock17_(
         block17_1, 2, ['947', '918', '906', '1069', '1148']);
-    const block17_3 = await this.buildBlock17_(
+    const block17_3 = this.buildBlock17_(
         block17_2, 3, ['1087', '932', '1028', '1150', '1137']);
-    const block17_4 = await this.buildBlock17_(
+    const block17_4 = this.buildBlock17_(
         block17_3, 4, ['1089', '1039', '1001', '1135', '1129']);
-    const block17_5 = await this.buildBlock17_(
+    const block17_5 = this.buildBlock17_(
         block17_4, 5, ['1123', '898', '1090', '1105', '920']);
-    const block17_6 = await this.buildBlock17_(
+    const block17_6 = this.buildBlock17_(
         block17_5, 6, ['935', '998', '1143', '1061', '1049']);
-    const block17_7 = await this.buildBlock17_(
+    const block17_7 = this.buildBlock17_(
         block17_6, 7, ['1118', '1023', '1013', '1092', '1093']);
-    const block17_8 = await this.buildBlock17_(
+    const block17_8 = this.buildBlock17_(
         block17_7, 8, ['1141', '1024', '1140', '929', '1120']);
-    const block17_9 = await this.buildBlock17_(
+    const block17_9 = this.buildBlock17_(
         block17_8, 9, ['1067', '908', '1142', '1003', '1074']);
-    const block17_10 = await this.buildBlock17_(
+    const block17_10 = this.buildBlock17_(
         block17_9, 10, ['1078', '913', '1138', '982', '967']);
 
     // Mixed 7a branches
-    const mixed7a_pool = this.builder_.maxPool2d(block17_10, poolOptions);
-    const mixed7a_branch0_0 = await this.buildConv_(
+    const mixed7a_pool = block17_10
+        .then((block17_10) => this.builder_.maxPool2d(block17_10, poolOptions));
+    const mixed7a_branch0_0 = this.buildConv_(
         block17_10, '1103', 'Mixed_7a_Branch_0_Conv2d_0a_1x1');
-    const mixed7a_branch0_1 = await this.buildConv_(
+    const mixed7a_branch0_1 = this.buildConv_(
         mixed7a_branch0_0, '1066',
         'Mixed_7a_Branch_0_Conv2d_1a_3x3', {strides});
-    const mixed7a_branch1_0 = await this.buildConv_(
+    const mixed7a_branch1_0 = this.buildConv_(
         block17_10, '1056', 'Mixed_7a_Branch_1_Conv2d_0a_1x1');
-    const mixed7a_branch1_1 = await this.buildConv_(
+    const mixed7a_branch1_1 = this.buildConv_(
         mixed7a_branch1_0, '1043',
         'Mixed_7a_Branch_1_Conv2d_1a_3x3', {strides});
-    const mixed7a_branch2_0 = await this.buildConv_(
+    const mixed7a_branch2_0 = this.buildConv_(
         block17_10, '1145', 'Mixed_7a_Branch_2_Conv2d_0a_1x1');
-    const mixed7a_branch2_1 = await this.buildConv_(
+    const mixed7a_branch2_1 = this.buildConv_(
         mixed7a_branch2_0, '1077',
         'Mixed_7a_Branch_2_Conv2d_0b_3x3', {autoPad});
-    const mixed7a_branch2_2 = await this.buildConv_(
+    const mixed7a_branch2_2 = this.buildConv_(
         mixed7a_branch2_1, '897',
         'Mixed_7a_Branch_2_Conv2d_1a_3x3', {strides});
-    const mixed7a = this.builder_.concat(
-        [mixed7a_branch0_1, mixed7a_branch1_1,
-          mixed7a_branch2_2, mixed7a_pool], 1);
+    const mixed7a = Promise.all([mixed7a_branch0_1, mixed7a_branch1_1,
+                                 mixed7a_branch2_2, mixed7a_pool])
+        .then((inputs) => this.builder_.concat(inputs, 1));
 
     // Block 8
-    const block8_1 = await this.buildBlock8_(
+    const block8_1 = this.buildBlock8_(
         mixed7a, 1, ['1065', '1126', '1110', '1116', '1107']);
-    const block8_2 = await this.buildBlock8_(
+    const block8_2 = this.buildBlock8_(
         block8_1, 2, ['999', '1144', '1102', '1097', '981']);
-    const block8_3 = await this.buildBlock8_(
+    const block8_3 = this.buildBlock8_(
         block8_2, 3, ['962', '934', '1064', '1052', '1042']);
-    const block8_4 = await this.buildBlock8_(
+    const block8_4 = this.buildBlock8_(
         block8_3, 4, ['915', '957', '1000', '1034', '1071']);
-    const block8_5 = await this.buildBlock8_(
+    const block8_5 = this.buildBlock8_(
         block8_4, 5, ['1139', '1114', '1106', '997', '970']);
-    const block8_6 = await this.buildBlock8_(
+    const block8_6 = this.buildBlock8_(
         block8_5, 6, ['977', '1104', '978', '1080', '1086'], false);
 
-    const averagePool = this.builder_.averagePool2d(block8_6);
+    const averagePool = this.builder_.averagePool2d(await block8_6);
     // Use reshape to implement squeeze(averagePool, {axes: [2, 3]});
     const squeezed_shape = averagePool.shape();
     squeezed_shape.splice(2, 2);
--- face_recognition/facenet_nhwc.js
+++ face_recognition/facenet_nhwc.js
@@ -29,9 +29,10 @@ export class FaceNetNhwc {
 
   async buildConv_(input, namePrefix, options = undefined, relu = true) {
     const weightsName = `${this.weightsUrl_}/${namePrefix}_kernel.npy`;
-    const weights = await buildConstantByNpy(this.builder_, weightsName);
     const biasName = `${this.weightsUrl_}/${namePrefix}_Conv2D_bias.npy`;
-    const bias = await buildConstantByNpy(this.builder_, biasName);
+    const [weights, bias] = await Promise.all(
+      [weightsName, biasName].map(
+          name => buildConstantByNpy(this.builder_, name)));
     if (options !== undefined) {
       options.inputLayout = 'nhwc';
       options.filterLayout = 'ohwi';
@@ -43,6 +44,9 @@ export class FaceNetNhwc {
         bias: bias,
       };
     }
+
+    input = await input;
+
     // WebNN spec drops autoPad support, compute the explicit padding instead.
     if (options.autoPad == 'same-upper') {
       options.padding =
@@ -56,58 +60,61 @@ export class FaceNetNhwc {
   }
 
   async buildBlock35_(input, indice) {
-    const branch0 = await this.buildConv_(
+    const branch0 = this.buildConv_(
         input, `Block35_${indice}_Branch_0_Conv2d_1x1`, {autoPad});
-    const branch1_0 = await this.buildConv_(
+    const branch1_0 = this.buildConv_(
         input, `Block35_${indice}_Branch_1_Conv2d_0a_1x1`, {autoPad});
-    const branch1_1 = await this.buildConv_(
+    const branch1_1 = this.buildConv_(
         branch1_0, `Block35_${indice}_Branch_1_Conv2d_0b_3x3`, {autoPad});
-    const branch2_0 = await this.buildConv_(
+    const branch2_0 = this.buildConv_(
         input, `Block35_${indice}_Branch_2_Conv2d_0a_1x1`, {autoPad});
-    const branch2_1 = await this.buildConv_(
+    const branch2_1 = this.buildConv_(
         branch2_0, `Block35_${indice}_Branch_2_Conv2d_0b_3x3`, {autoPad});
-    const branch2_2 = await this.buildConv_(
+    const branch2_2 = this.buildConv_(
         branch2_1, `Block35_${indice}_Branch_2_Conv2d_0c_3x3`, {autoPad});
 
-    const concat = this.builder_.concat([branch0, branch1_1, branch2_2], 3);
-    const conv = await this.buildConv_(
+    const concat = Promise.all([branch0, branch1_1, branch2_2])
+        .then((inputs) => this.builder_.concat(inputs, 3));
+    const conv = this.buildConv_(
         concat, `Block35_${indice}_Conv2d_1x1`, {autoPad}, false);
 
-    return this.builder_.relu(this.builder_.add(input, conv));
+    return this.builder_.relu(this.builder_.add(await input, await conv));
   }
 
   async buildBlock17_(input, indice) {
-    const branch0 = await this.buildConv_(
+    const branch0 = this.buildConv_(
         input, `Block17_${indice}_Branch_0_Conv2d_1x1`, {autoPad});
-    const branch1_0 = await this.buildConv_(
+    const branch1_0 = this.buildConv_(
         input, `Block17_${indice}_Branch_1_Conv2d_0a_1x1`, {autoPad});
-    const branch1_1 = await this.buildConv_(
+    const branch1_1 = this.buildConv_(
         branch1_0, `Block17_${indice}_Branch_1_Conv2d_0b_1x7`, {autoPad});
-    const branch1_2 = await this.buildConv_(
+    const branch1_2 = this.buildConv_(
         branch1_1, `Block17_${indice}_Branch_1_Conv2d_0c_7x1`, {autoPad});
 
-    const concat = this.builder_.concat([branch0, branch1_2], 3);
-    const conv = await this.buildConv_(
+    const concat = Promise.all([branch0, branch1_2])
+        .then((inputs) => this.builder_.concat(inputs, 3));
+    const conv = this.buildConv_(
         concat, `Block17_${indice}_Conv2d_1x1`, {autoPad}, false);
 
-    return this.builder_.relu(this.builder_.add(input, conv));
+    return this.builder_.relu(this.builder_.add(await input, await conv));
   }
 
   async buildBlock8_(input, indice, relu = true) {
-    const branch0 = await this.buildConv_(
+    const branch0 = this.buildConv_(
         input, `Block8_${indice}_Branch_0_Conv2d_1x1`, {autoPad});
-    const branch1_0 = await this.buildConv_(
+    const branch1_0 = this.buildConv_(
         input, `Block8_${indice}_Branch_1_Conv2d_0a_1x1`, {autoPad});
-    const branch1_1 = await this.buildConv_(
+    const branch1_1 = this.buildConv_(
         branch1_0, `Block8_${indice}_Branch_1_Conv2d_0b_1x3`, {autoPad});
-    const branch1_2 = await this.buildConv_(
+    const branch1_2 = this.buildConv_(
         branch1_1, `Block8_${indice}_Branch_1_Conv2d_0c_3x1`, {autoPad});
 
-    const concat = this.builder_.concat([branch0, branch1_2], 3);
-    const conv = await this.buildConv_(
+    const concat = Promise.all([branch0, branch1_2])
+        .then((inputs) => this.builder_.concat(inputs, 3));
+    const conv = this.buildConv_(
         concat, `Block8_${indice}_Conv2d_1x1`, {autoPad}, false);
 
-    let result = this.builder_.add(input, conv);
+    let result = this.builder_.add(await input, await conv);
 
     if (relu) {
       result = this.builder_.relu(result);
@@ -116,17 +123,17 @@ export class FaceNetNhwc {
   }
 
   async buildFullyConnected_(input) {
-    input = this.builder_.reshape(input, [1, 1792]);
-    const weights = await buildConstantByNpy(this.builder_,
+    const weights = buildConstantByNpy(this.builder_,
         `${this.weightsUrl_}/Bottleneck_kernel_transpose.npy`);
-    const bias = await buildConstantByNpy(this.builder_,
+    const bias = buildConstantByNpy(this.builder_,
         `${this.weightsUrl_}/Bottleneck_MatMul_bias.npy`);
+    input = this.builder_.reshape(await input, [1, 1792]);
     const options = {
       aTranspose: false,
       bTranspose: true,
-      c: bias,
+      c: await bias,
     };
-    return this.builder_.gemm(input, weights, options);
+    return this.builder_.gemm(input, await weights, options);
   }
 
   async load(contextOptions) {
@@ -139,77 +146,80 @@ export class FaceNetNhwc {
 
     const poolOptions = {windowDimensions: [3, 3], strides, layout: 'nhwc'};
 
-    const conv0 = await this.buildConv_(input, 'Conv2d_1a_3x3', {strides});
-    const conv1 = await this.buildConv_(conv0, 'Conv2d_2a_3x3');
-    const conv2 = await this.buildConv_(conv1, 'Conv2d_2b_3x3', {autoPad});
+    const conv0 = this.buildConv_(input, 'Conv2d_1a_3x3', {strides});
+    const conv1 = this.buildConv_(conv0, 'Conv2d_2a_3x3');
+    const conv2 = this.buildConv_(conv1, 'Conv2d_2b_3x3', {autoPad});
 
-    const pool0 = this.builder_.maxPool2d(conv2, poolOptions);
+    const pool0 = conv2.then((conv2) => this.builder_.maxPool2d(conv2, poolOptions));
 
-    const conv3 = await this.buildConv_(pool0, 'Conv2d_3b_1x1');
-    const conv4 = await this.buildConv_(conv3, 'Conv2d_4a_3x3');
-    const conv5 = await this.buildConv_(conv4, 'Conv2d_4b_3x3', {strides});
+    const conv3 = this.buildConv_(pool0, 'Conv2d_3b_1x1');
+    const conv4 = this.buildConv_(conv3, 'Conv2d_4a_3x3');
+    const conv5 = this.buildConv_(conv4, 'Conv2d_4b_3x3', {strides});
 
     // Block 35
-    const block35_1 = await this.buildBlock35_(conv5, 1);
-    const block35_2 = await this.buildBlock35_(block35_1, 2);
-    const block35_3 = await this.buildBlock35_(block35_2, 3);
-    const block35_4 = await this.buildBlock35_(block35_3, 4);
-    const block35_5 = await this.buildBlock35_(block35_4, 5);
+    const block35_1 = this.buildBlock35_(conv5, 1);
+    const block35_2 = this.buildBlock35_(block35_1, 2);
+    const block35_3 = this.buildBlock35_(block35_2, 3);
+    const block35_4 = this.buildBlock35_(block35_3, 4);
+    const block35_5 = this.buildBlock35_(block35_4, 5);
 
     // Mixed 6a branches
-    const mixed6a_branch0 = await this.buildConv_(
+    const mixed6a_branch0 = this.buildConv_(
         block35_5, 'Mixed_6a_Branch_0_Conv2d_1a_3x3', {strides});
-    const mixed6a_pool = this.builder_.maxPool2d(block35_5, poolOptions);
-    const mixed6a_branch1_0 = await this.buildConv_(
+    const mixed6a_pool = block35_5.then((block35_5) =>
+        this.builder_.maxPool2d(block35_5, poolOptions));
+    const mixed6a_branch1_0 = this.buildConv_(
         block35_5, 'Mixed_6a_Branch_1_Conv2d_0a_1x1', {autoPad});
-    const mixed6a_branch1_1 = await this.buildConv_(
+    const mixed6a_branch1_1 = this.buildConv_(
         mixed6a_branch1_0, 'Mixed_6a_Branch_1_Conv2d_0b_3x3', {autoPad});
-    const mixed6a_branch1_2 = await this.buildConv_(
+    const mixed6a_branch1_2 = this.buildConv_(
         mixed6a_branch1_1, 'Mixed_6a_Branch_1_Conv2d_1a_3x3', {strides});
-    const mixed6a = this.builder_.concat(
-        [mixed6a_branch0, mixed6a_branch1_2, mixed6a_pool], 3);
+    const mixed6a = Promise.all([mixed6a_branch0, mixed6a_branch1_2, mixed6a_pool])
+        .then((inputs) => this.builder_.concat(inputs, 3));
 
     // Block 17
-    const block17_1 = await this.buildBlock17_(mixed6a, 1);
-    const block17_2 = await this.buildBlock17_(block17_1, 2);
-    const block17_3 = await this.buildBlock17_(block17_2, 3);
-    const block17_4 = await this.buildBlock17_(block17_3, 4);
-    const block17_5 = await this.buildBlock17_(block17_4, 5);
-    const block17_6 = await this.buildBlock17_(block17_5, 6);
-    const block17_7 = await this.buildBlock17_(block17_6, 7);
-    const block17_8 = await this.buildBlock17_(block17_7, 8);
-    const block17_9 = await this.buildBlock17_(block17_8, 9);
-    const block17_10 = await this.buildBlock17_(block17_9, 10);
+    const block17_1 = this.buildBlock17_(mixed6a, 1);
+    const block17_2 = this.buildBlock17_(block17_1, 2);
+    const block17_3 = this.buildBlock17_(block17_2, 3);
+    const block17_4 = this.buildBlock17_(block17_3, 4);
+    const block17_5 = this.buildBlock17_(block17_4, 5);
+    const block17_6 = this.buildBlock17_(block17_5, 6);
+    const block17_7 = this.buildBlock17_(block17_6, 7);
+    const block17_8 = this.buildBlock17_(block17_7, 8);
+    const block17_9 = this.buildBlock17_(block17_8, 9);
+    const block17_10 = this.buildBlock17_(block17_9, 10);
 
     // Mixed 7a branches
-    const mixed7a_pool = this.builder_.maxPool2d(block17_10, poolOptions);
-    const mixed7a_branch0_0 = await this.buildConv_(
+    const mixed7a_pool = block17_10.then((block17_10) =>
+        this.builder_.maxPool2d(block17_10, poolOptions));
+    const mixed7a_branch0_0 = this.buildConv_(
         block17_10, 'Mixed_7a_Branch_0_Conv2d_0a_1x1', {autoPad});
-    const mixed7a_branch0_1 = await this.buildConv_(
+    const mixed7a_branch0_1 = this.buildConv_(
         mixed7a_branch0_0, 'Mixed_7a_Branch_0_Conv2d_1a_3x3', {strides});
-    const mixed7a_branch1_0 = await this.buildConv_(
+    const mixed7a_branch1_0 = this.buildConv_(
         block17_10, 'Mixed_7a_Branch_1_Conv2d_0a_1x1', {autoPad});
-    const mixed7a_branch1_1 = await this.buildConv_(
+    const mixed7a_branch1_1 = this.buildConv_(
         mixed7a_branch1_0, 'Mixed_7a_Branch_1_Conv2d_1a_3x3', {strides});
-    const mixed7a_branch2_0 = await this.buildConv_(
+    const mixed7a_branch2_0 = this.buildConv_(
         block17_10, 'Mixed_7a_Branch_2_Conv2d_0a_1x1', {autoPad});
-    const mixed7a_branch2_1 = await this.buildConv_(
+    const mixed7a_branch2_1 = this.buildConv_(
         mixed7a_branch2_0, 'Mixed_7a_Branch_2_Conv2d_0b_3x3', {autoPad});
-    const mixed7a_branch2_2 = await this.buildConv_(
+    const mixed7a_branch2_2 = this.buildConv_(
         mixed7a_branch2_1, 'Mixed_7a_Branch_2_Conv2d_1a_3x3', {strides});
-    const mixed7a = this.builder_.concat(
-        [mixed7a_branch0_1, mixed7a_branch1_1,
-          mixed7a_branch2_2, mixed7a_pool], 3);
+    const mixed7a = Promise.all([mixed7a_branch0_1, mixed7a_branch1_1,
+                                 mixed7a_branch2_2, mixed7a_pool])
+        .then((inputs) => this.builder_.concat(inputs, 3));
 
     // Block 8
-    const block8_1 = await this.buildBlock8_(mixed7a, 1);
-    const block8_2 = await this.buildBlock8_(block8_1, 2);
-    const block8_3 = await this.buildBlock8_(block8_2, 3);
-    const block8_4 = await this.buildBlock8_(block8_3, 4);
-    const block8_5 = await this.buildBlock8_(block8_4, 5);
-    const block8_6 = await this.buildBlock8_(block8_5, 6, false);
+    const block8_1 = this.buildBlock8_(mixed7a, 1);
+    const block8_2 = this.buildBlock8_(block8_1, 2);
+    const block8_3 = this.buildBlock8_(block8_2, 3);
+    const block8_4 = this.buildBlock8_(block8_3, 4);
+    const block8_5 = this.buildBlock8_(block8_4, 5);
+    const block8_6 = this.buildBlock8_(block8_5, 6, false);
 
-    const mean = this.builder_.averagePool2d(block8_6, {layout: 'nhwc'});
+    const mean = block8_6.then((block8_6) =>
+        this.builder_.averagePool2d(block8_6, {layout: 'nhwc'}));
     const fc = await this.buildFullyConnected_(mean);
     // L2Normalization will be handled in post-processing
     return fc;
